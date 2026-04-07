c6935861457cc2984a60a6d8b2b372dd1121f431
Toriki1111 <144874838+Toriki1111@users.noreply.github.com>
2026-04-07 21:45:14 +0700

    Update bot_stock.py

--- bot_stock.py
+++ bot_stock.py
@@ -12,7 +12,9 @@ WATCHLIST = {
     "Precious Metals": ["GC=F", "SI=F", "GOLD"]
 }
 
-
+def get_multi_sector_data():
+    now_utc = datetime.utcnow()
+    now_vn = now_utc + timedelta(hours=7)
     
     timestamp = now_vn.strftime('%d/%m/%Y %H:%M:%S')
     content = f"### 📊 Global Market Update - {timestamp}\n\n"
